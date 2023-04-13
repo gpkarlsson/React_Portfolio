c3e3ed3f03e0b6027dfb23a585de464b40e4a0b5
gordonk <gpkarlsson72@gmail.com>
2023-04-13 17:27:35 -0500

    working on main content

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css';
 import React from 'react';
 import Landing from './pages/Home/Landing.jsx'
 // import AboutMe from './pages/AboutMe'
-// import Header from './components/Header'
+// import Header from './components/Header/Header'
 import Navigation from './components/Navigation/Navigation'
+import Footer from './components/Footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
@@ -11,12 +12,20 @@ export default function App() {
       <main>
     <Navigation />
     <Landing />
+    <h1>Gordon Karlsson</h1>
+    <h3>Web Developer</h3>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+
+    </ul>
     {/* <Header /> */}
     {/* <AboutMe />  */}
     {/* <Contact />
     <Portfolio />
-    <Resume />
-    <Footer /> */}
+    <Resume />*/}
+    <Footer /> 
     
       </main>
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Footer() {
-    
+  return(
+    <ul>
+    <li><a href="#">GitHub</a></li>
+    <li><a href="#">LinkedIn</a></li>
+    <li><a href="#">Stack Overflow</a></li>
+  </ul>
+  );
+  
 }
