b7993eafa04ad9cccd69b060fe5102c35ee80ed9
gordonk <gpkarlsson72@gmail.com>
2023-04-13 18:02:16 -0500

    working on landing page, css

--- src/pages/Home/Landing.jsx
+++ src/pages/Home/Landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export default function Landing() {
   return (
@@ -10,22 +11,21 @@ export default function Landing() {
     <h1 id="h1">Gordon Karlsson</h1>
     <h3 id="h3">Web Developer</h3>
     <ul id="LINK_UL">
-      <li></li>
-      <li></li>
-      <li></li>
+      <li>Something</li>
+      <li>Something else?</li>
+      <li>Who Knows</li>
     </ul>
 
-    <Container>
-      <Row>
-        <Col sm={8}>sm=8</Col>
-        <Col sm={4}>sm=4</Col>
-      </Row>
-      <Row>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
-      </Row>
-    </Container>
+    <Card className="MAIN_CARD" style={{ width: '18rem'  }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>About Me ? Prob needs to be in the middle, huh?</Card.Title>
+        <Card.Text>
+         
+        </Card.Text>
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
     </>
   );
 }
